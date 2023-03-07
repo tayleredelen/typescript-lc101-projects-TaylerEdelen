0a2b6b82827aea79647b95cfa55058547f55b867
Tayler Edelen <TaylerLEdelen@gmail.com>
2023-03-06 18:58:44 -0600

    forgot to compile

--- studio/Astronaut.js
+++ studio/Astronaut.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 exports.Astronaut = void 0;
 var Astronaut = /** @class */ (function () {
     function Astronaut(massKg, name) {
-        this.massKg = massKg;
+        this.massKg = massKg; //sets values
         this.name = name;
     }
     return Astronaut;
--- studio/Cargo.js
+++ studio/Cargo.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 exports.Cargo = void 0;
 var Cargo = /** @class */ (function () {
     function Cargo(massKg, material) {
-        this.massKg = massKg;
+        this.massKg = massKg; //sets values
         this.material = material;
     }
     return Cargo;
